1fe4e181e953b6220d80ef88fa1bc06500bfbbae
DaAirman <160302643+gAirman@users.noreply.github.com>
2025-08-11 21:35:26 +0200

    Update const.py
    added ag_power_lithium_bms under BMS_TYPES

--- custom_components/bms_ble/const.py
+++ custom_components/bms_ble/const.py
@@ -12,6 +12,7 @@ from homeassistant.const import (  # noqa: F401  # pylint: disable=unused-import
 
 BMS_TYPES: Final[list[str]] = [
     "abc_bms",
+    "ag_power_lithium_bms",
     "cbtpwr_bms",
     "cbtpwr_vb_bms",
     "daly_bms",
